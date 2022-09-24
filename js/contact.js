5502feaf1c5bfbe65958bc1efc3e25989109be3a
Pilusjin <75672659+Ilusjin@users.noreply.github.com>
2022-09-24 18:32:23 +0200

    updated

---
+++ js/contact.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("#form");
+
+const fullname = document.querySelector("#name");
+const fullNameError = document.querySelector("#fullNameError");
+const address = document.querySelector("#message");
+const addressError = document.querySelector("#messageError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+
+    if (checkLength(fullname.value, 0) === true) {
+        fullNameError.style.display = "none";
+    } else {
+        fullNameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (checkLength(message.value, 0) === true) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+    }
+
+}
+
+form.addEventListener("onsubmit", form);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
